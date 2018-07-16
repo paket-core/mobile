d58415f956d13a3efa49c9cd3f85edeb0ad0b287
Andrey M <andrey.m@it-dimension.com>
2018-07-17 01:47:20 +0300

    #fix splash screen #profile UI #input fixes

--- PaketGlobal/Controls/PaketPicker.cs
+++ PaketGlobal/Controls/PaketPicker.cs
@@ -21,6 +21,15 @@ namespace PaketGlobal
         public static readonly BindableProperty ImageAlignmentProperty =
             BindableProperty.Create(nameof(ImageAlignment), typeof(ImageAlignment), typeof(PaketEntry), ImageAlignment.Right);
 
+        public static readonly BindableProperty BackgroundProperty =
+    BindableProperty.Create(nameof(BackgroundV), typeof(int), typeof(PaketEntry), 0);
+
+        public int BackgroundV
+        {
+            get { return (int)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
         public Color LineColor
         {
             get { return (Color)GetValue(LineColorProperty); }
