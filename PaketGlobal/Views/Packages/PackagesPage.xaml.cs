e7c25cf7f2bfdc46177aab212bc6490f4d4d35ab
Andrey M <andrey.m@it-dimension.com>
2018-07-16 14:10:19 +0300

    #remove debug keys

--- PaketGlobal/Views/Packages/PackagesPage.xaml.cs
+++ PaketGlobal/Views/Packages/PackagesPage.xaml.cs
@@ -161,9 +161,10 @@ namespace PaketGlobal
         {
             var newPackage = new Package()
             {
-                CourierPubkey="GAIDWM24Q6KKCH5PG7Z24B6ODUMCO4NH2APL4ASLMV75INOTQRNMG2CK",
-                RecipientPubkey="GBP7DJE4MHR5UY22NYHIMQDAUOMCY5YMRMQUPX5TFIEM74O4B4EHJKMB"
+                //CourierPubkey="GAIDWM24Q6KKCH5PG7Z24B6ODUMCO4NH2APL4ASLMV75INOTQRNMG2CK",
+                //RecipientPubkey="GBP7DJE4MHR5UY22NYHIMQDAUOMCY5YMRMQUPX5TFIEM74O4B4EHJKMB"
             };
+
 
             var packagePage = new LaunchPackagePage(newPackage);
             Navigation.PushAsync(packagePage);
