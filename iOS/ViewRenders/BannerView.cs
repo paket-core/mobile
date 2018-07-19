5b83e61b909d2eef9b683103e0f3a17beeb86df0
Andrey M <andrey.m@it-dimension.com>
2018-07-19 12:15:52 +0300

    #notifications

---
+++ iOS/ViewRenders/BannerView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace PaketGlobal.iOS.ViewRenders
+{
+    public partial class BannerView : UIView
+    {
+        public static readonly UINib Nib;
+
+        private Action _callback;
+
+        static BannerView()
+        {
+            Nib = UINib.FromName("BannerViewNib", NSBundle.MainBundle);
+        }
+
+        static public BannerView View()
+        {
+            var view = Runtime.GetNSObject(NSBundle.MainBundle.LoadNib("BannerViewNib", null, null).ValueAt(0)) as BannerView;
+            return view;
+        }
+
+        protected BannerView(IntPtr handle) : base(handle)
+        {
+            
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            MainView.BackgroundColor = UIColor.White.ColorWithAlpha(0.95f);
+
+            MainView.Layer.BorderColor = UIColor.White.CGColor;
+            MainView.Layer.CornerRadius = 15;
+
+            MainView.Layer.MasksToBounds = false;
+            MainView.Layer.ShadowOffset = new CGSize(-2, 2);
+            MainView.Layer.ShadowRadius = 4;
+            MainView.Layer.ShadowOpacity = 0.1f;
+        }
+
+        public void SetPackage(Package package, Action callback)
+        {
+            _callback = callback;
+        }
+
+        partial void OnClickBanner(UIButton sender)
+        {
+            _callback();
+        }
+    }
+}
---
+++ iOS/ViewRenders/BannerView.designer.cs
@@ -0,0 +1,29 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using UIKit;
+
+namespace PaketGlobal.iOS.ViewRenders
+{
+    [Register("BannerView")]
+    partial class BannerView
+    {
+        [Outlet]
+        UIView MainView { get; set; }
+        [Outlet]
+        UILabel TitleLabel { get; set; }
+        [Outlet]
+        UIImageView IconView { get; set; }
+
+        [Action("OnClickBanner:")]
+        partial void OnClickBanner(UIButton sender);
+
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
